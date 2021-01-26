875bae21212584f7f03b68cdf21429f8aaef97f6
Rok Mandeljc <rok.mandeljc@gmail.com>
2021-01-26 13:30:53 +0100

    utils/osx: add support for fat binaries to fix_exe_for_code_signing()
    On macOS, fix_exe_for_code_signing() function is used to make the
    frozen executable Mach-O format compliant by enlarging the LC_SYMTAB
    section/segment to cover the embedded .pkg archive.
    
    The current implementation assumes single-arch Mach-O thin binary,
    and needs to be extended to support fat binaries:
    * we need to modify the last available header/slice instead of
      the first one. In single-arch binary (thin or fat) this makes
      no difference, but in multi-arch fat binary, the last slice is
      the one that is adjacent to the embedded archive
    * we need to account for the header's offset. The offset always seem
      to be zero for header in the thin binary, but in fat binaries
      it is non-zero even if only single arch slice is present.
    * the total file size needs to be obtained from the file itself
      via stat. In fat binary, the reported header.size does not seem
      to include the appended data. (It makes more sense to do stat,
      anyway).
    * in a fat binary, we need to modify the fat header size for the
      architecture slice that we are modifying. Unfortunately, macholib
      does not seem to support that, so we need to do it ourselves.
    
    With these changes, it is now possible to codesign the executable
    produced from single-arch (x86_64) thin bootloader executable,
    or single-arch (x86_64) fat bootloader executable.

--- PyInstaller/utils/osx.py
+++ PyInstaller/utils/osx.py
@@ -85,9 +85,16 @@ def fix_exe_for_code_signing(filename):
     http://developer.apple.com/documentation/Darwin/Reference/ManPages/man5/Mach-O.5.html
     """
     exe_data = MachO(filename)
-    # Every load command is a tupple: (cmd_metadata, segment, [section1, section2])
-    cmds = exe_data.headers[0].commands  # '0' - Exe contains only one architecture.
-    file_size = exe_data.headers[0].size
+    # Take the last available header. The executable is either a single-arch
+    # thin Mach-O binary, or a universal2 fat binary. In the latter case,
+    # we want to adjust the symbol table of the last (arm64) slice.
+    header = exe_data.headers[-1]
+    # Every load command is a tuple: (cmd_metadata, segment, [section1, section2])
+    cmds = header.commands
+    # File size needs to be estimated from the file itself, because with
+    # fat binaries, header.size does not account for the appended data
+    # anymore...
+    file_size = os.path.getsize(filename)
 
     ## Make the embedded .pkg archive part of the Mach-O 'String Table'.
     # Data about 'String Table' is in LC_SYMTAB load command.
@@ -95,14 +102,39 @@ def fix_exe_for_code_signing(filename):
         if c[0].get_cmd_name() == 'LC_SYMTAB':
             data = c[1]
             # Increase the size of 'String Table' to cover the embedded .pkg file.
-            new_strsize = file_size - data.stroff
+            new_strsize = file_size - (header.offset + data.stroff)
             data.strsize = new_strsize
     ## Fix the size of the __LINKEDIT segment.
     # __LINKEDIT segment data is the 4th item in the executable.
     linkedit = cmds[3][1]
-    new_segsize = file_size - linkedit.fileoff
+    new_segsize = file_size - (header.offset + linkedit.fileoff)
     linkedit.filesize = new_segsize
     linkedit.vmsize = new_segsize
+
     ## Write changes back.
     with open(exe_data.filename, 'rb+') as fp:
         exe_data.write(fp)
+
+    ## In fat binaries, we also need to adjust the fat header.
+    # At the time of writing, macholib 1.14 does not support this, so
+    # we need to do it ourselves...
+    if exe_data.fat:
+        from macholib.mach_o import FAT_MAGIC, FAT_MAGIC_64
+        from macholib.mach_o import fat_header, fat_arch, fat_arch64
+        with open(exe_data.filename, 'rb+') as fp:
+            # Taken from MachO.load_fat() implementation. The fat
+            # header's signature has already been validated when we
+            # loaded the file for the first time.
+            fat = fat_header.from_fileobj(fp)
+            if fat.magic == FAT_MAGIC:
+                archs = [fat_arch.from_fileobj(fp) for i in range(fat.nfat_arch)]
+            elif fat.magic == FAT_MAGIC_64:
+                archs = [fat_arch64.from_fileobj(fp) for i in range(fat.nfat_arch)]
+            # Adjust the size in the fat header for the last slice
+            arch = archs[-1]
+            arch.size = file_size - arch.offset
+            # Now write the fat headers back to the file
+            fp.seek(0)
+            fat.to_fileobj(fp)
+            for arch in archs:
+                arch.to_fileobj(fp)
