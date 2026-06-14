3fcfcca63eb26a77cbd9daf8777867ae6f5a2dbb
Rok Mandeljc <rok.mandeljc@gmail.com>
2026-06-14 18:19:04 +0200

    Basic support for python 3.16
    Bump the python version limits to allow installing under python 3.16,
    add `is_py316` to `PyInstaller.compat`, and update README.

--- PyInstaller/compat.py
+++ PyInstaller/compat.py
@@ -74,6 +74,7 @@ is_py312 = sys.version_info >= (3, 12)
 is_py313 = sys.version_info >= (3, 13)
 is_py314 = sys.version_info >= (3, 14)
 is_py315 = sys.version_info >= (3, 15)
+is_py316 = sys.version_info >= (3, 16)
 
 is_win = sys.platform.startswith('win')
 is_win_10 = is_win and (platform.win32_ver()[0] == '10')
